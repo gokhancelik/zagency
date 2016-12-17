2716995f1b40d9b7a68219aff669279cc1d5774f
gokhancelik <gokhancelik987@gmail.com>
2016-12-17 22:48:43 +0300

    tourprogram add generic add componente geçirme

--- src/app/pages/tourPrograms/tourProgram.add.component.ts
+++ src/app/pages/tourPrograms/tourProgram.add.component.ts
@@ -7,12 +7,12 @@ import { AddComponent } from '../../core/add.component';
 import { TourProgram } from '../../shared/models/tourProgram.model';
 import { TourProgramService } from '../../shared/services/index';
 import { DynamicFormControlModel, DynamicFormService } from '@ng2-dynamic-forms/core';
-import {TOURPROGRAM_FORM_MODEL} from './tour-form.model'
+import { TOURPROGRAM_FORM_MODEL } from './tour-form.model';
 
 
 @Component({
     selector: 'tourProgram-add',
-   encapsulation: ViewEncapsulation.None,
+    encapsulation: ViewEncapsulation.None,
     templateUrl: '../../core/form.component.html'
 })
 export class TourProgramAddComponent extends AddComponent<TourProgram>  {
