808bff84e17be2d4ff8af2cf8ca073d0417eedaa
vahapxxy <clkvhp@gmail.com>
2016-12-17 23:15:14 +0300

    a

--- src/app/pages/tourPhotos/tourPhoto.module.ts
+++ src/app/pages/tourPhotos/tourPhoto.module.ts
@@ -7,10 +7,6 @@ import {
 } from './index';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CarouselModule, ProgressbarModule } from 'ng2-bootstrap/ng2-bootstrap';
-<<<<<<< HEAD
-=======
-import { FormsModule, ReactiveFormsModule, } from '@angular/forms';
->>>>>>> 12faa10e5ce2d8e38ba77d581fc5435f4f580406
 import { FileUploadModule } from 'ng2-file-upload/ng2-file-upload';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { NgaModule } from '../../theme/nga.module';
@@ -23,11 +19,7 @@ import {
     DynamicFormsBootstrapUIModule
 } from '@ng2-dynamic-forms/ui-bootstrap';
 @NgModule({
-<<<<<<< HEAD
-    imports: [routing,DynamicFormsBootstrapUIModule, DynamicFormsCoreModule,RouterModule, ModalModule, CommonModule, FormsModule,
-=======
-    imports: [routing, RouterModule, ModalModule, CommonModule, FormsModule, ReactiveFormsModule,
->>>>>>> 12faa10e5ce2d8e38ba77d581fc5435f4f580406
+    imports: [routing, RouterModule, ModalModule, CommonModule, FormsModule,ReactiveFormsModule,
         CarouselModule, FileUploadModule, ProgressbarModule,
         Ng2SmartTableModule, NgaModule,ReactiveFormsModule],
     exports: [TourPhotoAddComponent, TourPhotoEditComponent, TourPhotoListComponent],
