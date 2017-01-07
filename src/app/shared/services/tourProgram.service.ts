63d485b56d1f43d7eb9ce535041957443fc2bc77
vahapxxy <clkvhp@gmail.com>
2017-01-08 01:32:45 +0300

    tourProgram was updated

--- src/app/shared/services/tourProgram.service.ts
+++ src/app/shared/services/tourProgram.service.ts
@@ -1,3 +1,4 @@
+import { TourService } from './tour.service';
 import { AuthService } from './../../security/auth.service';
 import { TourSchedule, Tour, TourProgram } from '../models';
 import { CompanyService } from './';
@@ -7,9 +8,13 @@ import { AngularFireDatabase, FirebaseRef } from 'angularfire2';
 import { BaseFirebaseService } from './base.firebase.service';
 @Injectable()
 export class TourProgramService extends BaseFirebaseService<TourProgram> {
-    constructor(private _af: AngularFireDatabase, private _authService: AuthService,
+    constructor(private _af: AngularFireDatabase,private tourService:TourService, private _authService: AuthService,
         @Inject(FirebaseRef) fb) {
         super(_af, 'tourPrograms', fb, _authService);
+    }
+        public mapRelationalObject(obj: TourProgram) {
+        obj.tourObj = this.tourService.getByKey(obj.tour);
+        return obj;
     }
     public fromJson(obj) {
         return TourProgram.fromJson(obj);
@@ -24,5 +29,13 @@ export class TourProgramService extends BaseFirebaseService<TourProgram> {
             .map(TourProgram.fromJsonList);
         return ts$;
     }
+       public getByKey(key): Observable<TourProgram> {
+        let that = this;
+        const tourProgram$ = this._af.object(this.getRoute() + '/' + key)
+            .map(this.fromJson)
+            .map(t => { return that.mapRelationalObject(t); });
+        return tourProgram$;
+    }
+  
 }
 
