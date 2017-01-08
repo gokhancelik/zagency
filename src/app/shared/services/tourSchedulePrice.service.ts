1bdece361e5d5d598ff8d31d344a38b1b60b9253
gokhancelik <gokhancelik987@gmail.com>
2017-01-08 06:30:27 +0300

    added  currency to tsp

--- src/app/shared/services/tourSchedulePrice.service.ts
+++ src/app/shared/services/tourSchedulePrice.service.ts
@@ -1,3 +1,5 @@
+import { PriceTypeService } from './priceType.service';
+import { CurrencyTypeService } from './currencyType.service';
 import { Observable } from 'rxjs/Rx';
 import { AuthService } from './../../security/auth.service';
 import { Tour, TourSchedule, TourSchedulePrice, PriceType, CurrencyType } from '../models';
@@ -8,8 +10,15 @@ import { BaseFirebaseService } from './base.firebase.service';
 @Injectable()
 export class TourSchedulePriceService extends BaseFirebaseService<TourSchedulePrice> {
     constructor(private _af: AngularFireDatabase, private _authService: AuthService,
+        private currencyTypeService: CurrencyTypeService,
+        private priceTypeService: PriceTypeService,
         @Inject(FirebaseRef) fb) {
         super(_af, 'tourSchedulePrices', fb, _authService);
+    }
+    public mapRelationalObject(obj: TourSchedulePrice) {
+        obj.currencyTypeObj = this.currencyTypeService.getByKey(obj.currency);
+        obj.priceTypeObj = this.priceTypeService.getByKey(obj.priceType);
+        return obj;
     }
     public fromJson(obj) {
         return TourSchedulePrice.fromJson(obj);
@@ -18,9 +27,11 @@ export class TourSchedulePriceService extends BaseFirebaseService<TourSchedulePr
         return TourSchedulePrice.fromJsonList(array);
     }
     getByTourScheduleKey(tsKey): Observable<TourSchedulePrice[]> {
+        let that = this;
         const ts$ = this._af.list(this.getRoute(),
             { query: { orderByChild: 'tourSchedule', equalTo: tsKey } })
-            .map(TourSchedulePrice.fromJsonList);
+            .map(this.fromJsonList)
+            .map(ts => ts.map(t => { return that.mapRelationalObject(t); }));
         return ts$;
     }
 }
